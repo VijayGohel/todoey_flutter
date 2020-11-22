7f54515c0327e9988abc2e3062d00c3375713342
vijay gohel <gohil5898@gmail.com>
2020-11-22 15:54:43 +0530

    listtile editing

--- lib/widgets/list_tile.dart
+++ lib/widgets/list_tile.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChnaged = false;
+
+  void changeState(bool changedValue) {
+    setState(() {
+      isChnaged = changedValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a note'),
-      trailing: Checkbox(value: false, onChanged: null),
+      title: Text(
+        'This is a note',
+        style: TextStyle(
+          decoration: isChnaged ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Mycheckbox(
+        valueChanged: isChnaged,
+        changeState: changeState,
+      ),
+    );
+  }
+}
+
+class Mycheckbox extends StatelessWidget {
+  final bool valueChanged;
+  final Function changeState;
+  Mycheckbox({this.valueChanged, this.changeState});
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: valueChanged,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: changeState,
     );
   }
 }
